b0f87abab48f9fe11baa436808254a4fdaa57f7c
bulby14 <104141521+bulby14@users.noreply.github.com>
2022-09-09 13:13:58 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="softi-xmcd-view",
-    version=1.3,
+    version=1.4,
     packages=find_packages(include=['xmcd_gui_logic_2', 'xmcd_gui', 'resourc', 'command_line', 'main_1']),
-    scripts=['command_line'],
+    
     install_requires = [
         'PyQt5',
         'scikit-image',
